1b38f1696e88766d3e508b7d2394d2c34d0c45c2
unknown <mathiasimagine@gmail.com>
2022-06-14 14:37:35 -0300

    feat add Counter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import Content from "./components/Content"
-import Header from "./components/Header"
-import Total from "./components/Total"
-import course from "./data"
+import { useState } from "react"
+import Display from "./components/Display"
+
 const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const setToZero = () => setCounter(0)
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total total={course.parts.length} />
-  
+      <Display counter={counter}/>
+      <button onClick={increaseByOne}>
+        plus
+      </button>
+      <button onClick={setToZero}> 
+        zero
+      </button>
     </div>
   )
 }
