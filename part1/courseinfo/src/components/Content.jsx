dbd7328dbe2e0e1cbeeb606d1fa4cc62dbe199b1
mathyled <mathiasimagine@gmail.com>
2025-06-06 09:17:08 -0300

    course information step one

---
+++ part1/courseinfo/src/components/Content.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Content = (props) => {
+  const { part, exercises } = props;
+  return (
+    <>
+      <p>
+        {part} {exercises}
+      </p>
+    </>
+  );
+};
+
+export default Content;
---
+++ part1/courseinfo/src/components/Total.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Total = ({total}) => {
+  return (
+    <>
+      <p>Numbers of exercixes {total}</p>
+    </>
+  );
+};
+
+export default Total;
